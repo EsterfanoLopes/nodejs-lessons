242edfec5da1859a35438dab6a0ac05ffedae8d8
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-29 23:27:45 -0200

    Session 2. Lecture 13. Understanding arrow functions

--- play.js
+++ play.js
@@ -4,15 +4,26 @@ const hasHobbies = true;
 // Do not never assign const constants
 age = 30;
 
-function summerizeUser(userName, userAge, userHasHobby) {
+// General function syntax
+const summarizeUser = (userName, userAge, userHasHobby) => {
     return (
-    'Name is ' + 
-    userName + 
-    ', age is ' + 
-    userAge + 
-    ' and the user has hobbies: ' +
-    userHasHobby
+        'Name is ' + 
+        userName + 
+        ', age is ' + 
+        userAge + 
+        ' and the user has hobbies: ' +
+        userHasHobby
     );
-}
+};
+console.log(summarizeUser(name, age, hasHobbies));
 
-console.log(summerizeUser(name, age, hasHobbies));
+// Short syntax to one line function. This will return a + b;
+const add = (a, b) => a + b;
+console.log(add(1, 2));
+
+// Short tag to one line function with one argument only. This will return a++
+const addOne = a => a + 1;
+console.log(addOne(1));
+
+const addRandom = () => 3 + 2;
+console.log(addRandom());
