bb93c5f31231f58d9974d4ff9cd80b4c1d4ae6c1
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-29 23:36:32 -0200

    Session 2. Lecture 15. Arrays & Arrays Methods

---
+++ arrays.js
@@ -0,0 +1,10 @@
+const hobbies = ['sports', 'cooking'];
+
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+
+// Functions of array. Using short function with one argument in .map
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// original array
+console.log(hobbies);
