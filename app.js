e503c2067893b31e0507bf01e55b0f0412603c82
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-31 21:38:40 -0200

    Section 5. Lecture 63. Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-    console.log("This always run!");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/add-product", (req, res, next) => {
-    res.send('<h1>The Add Product Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use("/", (req, res, next) => {
