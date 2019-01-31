a850f9eaa344a1dba96e74abd951a3169fbb4d2b
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-31 14:41:54 -0200

    Section 5. Lecture 60. How Middlewere Works

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
     console.log('In another Middlewere');
-    // ...
+    res.send('<h1>Hello from Express</h1>');
 });
 const server = http.createServer(app);
 
