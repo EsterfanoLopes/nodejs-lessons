640872bbd876a05fabe6c7b588295b622a6531fa
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-31 21:52:14 -0200

    Section 5. Lecture 65. Using Express Router

--- app.js
+++ app.js
@@ -3,19 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-product", (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use("/", (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
