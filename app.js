bab977714385b5ec12efd9f63205631c9f3b2411
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-30 23:41:39 -0200

    Section 3. Lectuture 34. Understanding Event Driven Code Execution

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       fs.writeFileSync('message.txt', message);
