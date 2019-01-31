a016eb7352c9ec1ac1d3383f441e961b13192379
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-31 21:28:50 -0200

    Section 5. Lecture 62. Handling Different Routes

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the Middlewere');
-    next(); // Allows the request to continue to the next middlewere in line
+app.use("/", (req, res, next) => {
+    console.log("This always run!");
+    next();
 });
-app.use((req, res, next) => {
-    console.log('In another Middlewere');
+
+app.use("/add-product", (req, res, next) => {
+    res.send('<h1>The Add Product Page</h1>');
+});
+
+app.use("/", (req, res, next) => {
     res.send('<h1>Hello from Express</h1>');
 });
 
