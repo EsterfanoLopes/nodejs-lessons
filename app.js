042758a31f7e2879fd9062ca27460f6966948c26
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-31 14:38:41 -0200

    Section 5. Lecture 59. Adding Middlewere

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the Middlewere');
+    next(); // Allows the request to continue to the next middlewere in line
+});
+app.use((req, res, next) => {
+    console.log('In another Middlewere');
+    // ...
+});
 const server = http.createServer(app);
 
 server.listen(3000);
