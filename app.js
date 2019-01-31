66074290f06bcefb599fc37311aca37b447e69ed
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-01-30 22:14:21 -0200

    Section 3. Lecture 29. Sending Responses

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+
+    // Writing and sending back the Response
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000);
