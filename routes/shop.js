711a05be037a547ce7b63ffb04ca698323c8008d
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-01 17:09:05 -0300

    Section 6. Lecture 80. Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,11 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', { 
+        prods: products, 
+        docTitle: 'Shop' 
+    });
 });
 
 module.exports = router;
