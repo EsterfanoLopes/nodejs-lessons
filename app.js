ef907484c5745a5bdae9263593a275228c8e35fc
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-01 10:13:24 -0300

    Section 5. Lecture 73. Serving Files Statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Filter only /admin paths
 app.use('/admin', adminRoutes);
