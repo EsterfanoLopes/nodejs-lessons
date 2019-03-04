d84f29317dbf9a94f050b3a7e0beaf57800545a3
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-04 14:48:35 -0300

    Section 6. Lecture 85. Working with Handlebars

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
