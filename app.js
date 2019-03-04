bf7a132f7137ede91e55840f5eb28a0d8971573f
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-04 15:34:11 -0300

    Section 6. Lecture 87. Adding the Layout to Handlebars

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'Shop', 
+    path: '/', 
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
