6016ded6afd5f46a2432d9b1cac172d868e10f1b
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 10:31:40 -0300

    Section 7. Lecture 97. Storing Data in Files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +7,36 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+        
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
     }
 }
