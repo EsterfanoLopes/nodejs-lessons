cae1c7af8b0ca3ba28ea3d6e3a97d02a85851531
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 10:44:57 -0300

    Section 7. Lecture 98. Fetching Data From Files Via the Model

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,14 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    const products = Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
   }
--- models/product.js
+++ models/product.js
@@ -25,18 +25,18 @@ module.exports = class Product {
         });
     }
 
-    static fetchAll() {
+    static fetchAll(cb) {
         const p = path.join(
             path.dirname(process.mainModule.filename), 
             'data', 
             'products.json'
         );
-        
+
         fs.readFile(p, (err, fileContent) => {
             if (err) {
-                return [];
+                cb([]);
             }
-            return JSON.parse(fileContent);
+            cb(JSON.parse(fileContent));
         });
     }
 }
