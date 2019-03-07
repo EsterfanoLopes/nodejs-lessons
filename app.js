2c59f9a8fb62a845b6385df3d09c16753d41a3c5
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 10:00:09 -0300

    Section 7. Lecture 95. Finishing the Controllers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path: ""});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
