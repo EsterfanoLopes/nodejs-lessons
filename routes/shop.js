f594aea55161be9b5dcb151d9a16e316473993cc
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 17:02:47 -0300

    Section 9. Lecture 118. Passing Data with POST requests

--- routes/shop.js
+++ routes/shop.js
@@ -17,6 +17,8 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
