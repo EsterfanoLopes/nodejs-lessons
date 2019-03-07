82251b2c033267b68ecf6e48446a7ff32113f901
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 10:09:06 -0300

    Section 7. Lecture 96. Adding a Product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require ('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { 
@@ -11,11 +11,13 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products, 
         pageTitle: 'Shop', 
