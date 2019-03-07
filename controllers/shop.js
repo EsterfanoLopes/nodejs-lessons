d7df03ac3dbe403ee2da561416940d5b048bc078
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-07 15:36:06 -0300

    Section 9. Lecture 117. Rendering the Product Detail View

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   console.log(Product.finById(prodId, product => {
-    console.log(product);
+    res.render('shop/product-detail', { 
+      product: product, 
+      pageTitle: product.title, 
+      path: '/products'
+    });
   }));
-  res.redirect('/');
 }
 
 exports.getIndex = (req, res, next) => {
