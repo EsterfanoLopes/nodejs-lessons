b355f4cc06674cdb345be38da45eff205b966666
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 20:47:42 -0300

    Section 10. Lecture 138. Retrieviing Data

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products').then().catch();
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result[0], result[1]);
+    }).catch(err => {
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
