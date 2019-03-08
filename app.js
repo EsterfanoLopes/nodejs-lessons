c23f65d63abdf24a1f2c642769c762e65cf3e46d
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 16:57:49 -0300

    Section 10. Lecture 137. Basic SQL and Creating a Table

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELECT * FROM products').then().catch();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
