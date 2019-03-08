a70e883e9907455e91feaf64d575ce68c76735a0
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 20:31:33 -0300

    Fixing mysql container connection

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'db',
+    port: '3306',
     user: 'root',
     password: 'root',
     database: 'node_complete'
