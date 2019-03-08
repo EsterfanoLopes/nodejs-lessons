892c2fc8dafd4bd04f77108dd4a1cf0b2b1cba1f
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 09:33:34 -0300

    Section 9. Lecture 119. Adding a Cart Model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -40,6 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.finById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   console.log(prodId);
   res.redirect('/cart');
 }
