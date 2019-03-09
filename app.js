c5ede4a72abcb94ffe05dcda0a122fe2c7157b40
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-09 01:48:30 -0300

    Section 11. Lecture 163. Creating and Fetching a Cart

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ sequelize
     return user;
 }).then(user => {
     // console.log(user);
+    return user.createCart();
+}).then(cart => {
     app.listen(3000);
 }).catch(err => {
     console.log(err);
--- controllers/shop.js
+++ controllers/shop.js
@@ -46,25 +46,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getProducts(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
-        if (cartProductData) {
-          cartProducts.push({
-            productData: product, 
-            qty: cartProductData.qty
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart
+        .getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
           });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    });
-  });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
