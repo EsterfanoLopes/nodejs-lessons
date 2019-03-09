64b9ebab34f06beb13b028cfcc252a04a39af369
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 23:11:14 -0300

    Section 11. Lecture 154. Fetching Admin Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -72,11 +72,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.findAll().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  }).catch(err => console.log(err));
 };
