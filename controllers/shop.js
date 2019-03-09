e1b252f63745a84fe60f74aacce290ebb357ad7c
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 21:47:50 -0300

    Section 10. Lecture 140. Fetching Products - Time to Practice

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,13 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  }).catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
