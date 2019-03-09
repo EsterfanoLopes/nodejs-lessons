30e040dc8ee56477d3db6324989c7ab11b7b8262
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 21:54:35 -0300

    Section 10. Lecture 141. Insertind Data into the Database

--- models/product.js
+++ models/product.js
@@ -22,7 +22,8 @@ module.exports = class Product {
   }
 
   save() {
-    
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+    [ this.title, this.price, this.imageUrl, this.description ]);
   }
 
   static deleteById(prodId) {
