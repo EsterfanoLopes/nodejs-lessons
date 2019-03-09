9eae6c7dd253a7f33e9c33d93fe0062634b9d31b
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-09 15:59:43 -0300

    Section 12. Lecture 189. Fixing the 'Add Product' Functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, new ObjectId(prodId));
+  const product = new Product(title, price, description, imageUrl, null);
   product.save()
     .then(result => {
       console.log('Created product.');
