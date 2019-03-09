3d6ea80b37a55f1e13b3ceaec35d2aeea30788ea
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 22:38:42 -0300

    Section 11. Lecture 150. Inserting Data and Creating a Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,9 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => {
-    res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  }).then(result => {
+    // console.log(result);
+    console.log('Created product ' + result.dataValues.title);
   }).catch(err => console.log(err));
   
 };
