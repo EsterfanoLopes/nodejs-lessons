0c3b6b8b7ff28ad3ffdef5c45db450a7619f964c
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 21:40:51 -0300

    Section 10. Lecture 139. Fetching Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,13 +23,14 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
-  });
+  }).catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
