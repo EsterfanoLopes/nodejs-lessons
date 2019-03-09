c05725c34b402f5369dbd538689c88f569310dd3
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-09 12:58:31 -0300

    Section 12. Lecture 177. Working with NoSQL and Using MongoDB

--- util/database.js
+++ util/database.js
@@ -1,10 +1,25 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_complete','root','root', {
-        dialect: 'mysql', 
-        host: 'db',
-        port: '3306'
-    }
-);
+// const sequelize = new Sequelize('node_complete','root','root', {
+//         dialect: 'mysql', 
+//         host: 'db',
+//         port: '3306'
+//     }
+// );
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+    MongoClient
+    .connect('mongodb+srv://nodelessons:nodelessons@node-lessons-wdsqh.mongodb.net/test?retryWrites=true')
+    .then(result => {
+        console.log('connected!')
+        callback(result);
+    })
+    .catch(err => console.log(err));
+}
+
+module.exports = mongoConnect;
