ed8debf85d8b637b9af61be2626bd85951bcab1a
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-09 17:13:48 -0300

    Section 12. Leccture 192. Working on Cart Items and Orders

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
@@ -24,6 +26,19 @@ class User {
     return dbOp
       .then(result => console.log('Operation executed'))
       .catch(err => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    
+    const updatedCart = { items: [{...product, quantity: 1}] };
+    const db = getDb();
+    return db.collection('users').updateOne(
+      {_id: new mongodb.ObjectId(this._id)}, 
+      {$set: { cart: updatedCart }}
+    );
   }
 
   static findById(userId) {
