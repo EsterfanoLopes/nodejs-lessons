303662c92e32c016d61b00e2052c6a8d50406c7f
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-09 02:22:33 -0300

    Section 11. Lecture 166. Deleting Related Items and Deleting Cart Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -100,10 +100,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.finById(prodId, product => {
-      Cart.deleteProduct(prodId, product.price);
-      res.redirect('/cart');
-    });
+    req.user
+      .getCart()
+      .then(cart => {
+        return cart.getProducts({ where: { id: prodId } });
+      })
+      .then(products => {
+        const product = products[0];
+        return product.cartitem.destroy();
+      })
+      .then(result => {
+        res.redirect('/cart');
+      })
+      .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
