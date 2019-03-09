74e54359a032b0f8a14094030a7b19210f9271d8
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-08 22:29:42 -0300

    Section 11. Lecture 149. Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -22,4 +22,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result => {
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+});
