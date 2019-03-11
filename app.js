42d85b02242deab51f3a90a80ad537bb6321a317
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 17:01:28 -0300

    Section 13. Lecture 214. Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,13 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5c8418a6a11d5576060972ce')
-//     .then(user => {
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       next();
-//     }).catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5c86bdbd535fb3019f8c8271')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +37,18 @@ mongoose
     'mongodb+srv://nodelessons:nodelessons@node-lessons-wdsqh.mongodb.net/shop?retryWrites=true'
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'nodelessons',
+          email: 'nodelessons@mail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [{
+      productId: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }]
+  }
+});
+
+module.exports = mongoose.model('user', userSchema);
+
 // const mongodb = require('mongodb');
 
 // const ObjectId = mongodb.ObjectId;
