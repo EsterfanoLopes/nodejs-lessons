9092be47643b379a37011181c6381e3c18b5258d
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 16:43:56 -0300

    Section 13. Lecture 211. Fetching a Single Product

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,15 +17,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
