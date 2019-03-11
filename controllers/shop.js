4594c22bae6027399fb8da835cbcc01afcfde669
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 18:21:24 -0300

    Section 13. Lecture 223. Getting and Displaying the Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -111,8 +111,8 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order
+    .find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
