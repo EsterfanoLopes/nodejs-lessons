b91a54b3e3ab7e84277d2b7bb94b432fbdbb0847
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 16:41:51 -0300

    Section 13. Lecture 210. Fetching All Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product
+    .find()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -37,7 +38,8 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product
+    .find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
--- routes/shop.js
+++ routes/shop.js
@@ -6,9 +6,9 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-// router.get('/', shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-// router.get('/products', shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
 // router.get('/products/:productId', shopController.getProduct);
 
