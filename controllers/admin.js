afde35c49f90c1350e61057904002c769458691c
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 16:52:20 -0300

    Section 13. Lecture 213. Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,7 +89,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
