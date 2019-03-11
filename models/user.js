922f6bedafcc22327829765884ef48121b971508
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-11 17:43:04 -0300

    Section 13. Lecture 219. Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -49,6 +49,14 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 }
 
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
