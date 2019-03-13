676dd6f16e3d9b0505d795dba911af0d4d465596
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-13 10:43:18 -0300

    Section 14. Lecture 231. Setting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').trim().split('=')[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isAuthenticated
+        isAuthenticated: isLoggedIn
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggeIn=true');
     res.redirect('/');
 };
