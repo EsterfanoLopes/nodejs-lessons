8e3bc4aa26051afc47df9beda737dada0e0fdfc6
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-13 17:35:38 -0300

    Section 14. Lecture 241. Making 'Add to Cart' Work Again

--- app.js
+++ app.js
@@ -46,6 +46,18 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+})
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -9,13 +9,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+  
 };
 
 exports.postLogout = (req, res, next) => {
