3b80c3882548e252f6cf3bcceec94dc618d6ff74
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-03-13 11:11:28 -0300

    section 14. Lecture 236. Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
+    // const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggeIn=true; HttpOnly');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
