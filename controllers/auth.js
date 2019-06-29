0405613f693ee9b1ce8fc5adcc3b8e540a774432
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-06-29 17:54:58 -0300

    Lecture 254. Adding the signin functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,16 +22,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+      if (!user) {
+        return res.redirect('/login');
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        }).catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    });
 };
 
 exports.postSignup = (req, res, next) => {
