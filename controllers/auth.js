57bbbe1cc6d4e492217af87d42f1744402e9e16a
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-06-29 16:21:48 -0300

    Section 15. Our updated project status

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,17 +8,28 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {
-  User.findById('5c86bdbd535fb3019f8c8271')
-    .then(user => {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-        req.session.save((err) => {
-            console.log(err);
-            res.redirect('/');
-        });
-    })
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
