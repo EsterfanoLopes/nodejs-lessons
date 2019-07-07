c63a88fcf98fb1ba02cb61c80ef665fe242551e9
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-07-07 01:08:07 -0300

    Lecture 278. Adding Logic to Update the Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -180,12 +180,40 @@ exports.getNewPassword = (req, res, next) => {
       message = null;
     }
 
-    res.render('auth/reset', {
+    res.render('auth/new-password', {
       path: '/new-password',
       pageTitle: 'New Password',
       errorMessage: message,
       userId: user._id.toString(),
+      passwordToken: token,
     });
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser 
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: {$gt: Date.now() },
+    _id: userId,
+  })
+  .then(user => {
+    resetUser = user;
+    return bcrypt.hash(newPassword, parseInt(process.env.ENCRYPT_ROUNDS));
+  })
+  .then(hashPassword => {
+    resetUser.password = hashPassword;
+    resetUser.resetToken = null;
+    resetUser.resetTokenExpiration = null;
+    return resetUser.save();
+  })
+  .then(result => {
+    res.redirect('/login');
   })
   .catch(err => console.log(err));
 };
