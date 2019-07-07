c4fee50010ac0394e0530541ccc595a15d39c004
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-07-07 00:16:40 -0300

    Lecture 275. Implementing Token Logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
@@ -129,4 +131,37 @@ exports.getReset = (req, res, next) => {
     pageTitle: 'Reset Password',
     errorMessage: message,
   });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'No account with that e-mail found');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration - Date.now() + 3600000;
+      user.save();
+    })
+    .then(result => {
+      res.redirect('/');
+      transporter.sendMail({
+        to: req.body.email,
+        from: 'shop@node-complete.com',
+        subject: 'Password Reset',
+        html: `
+          <p>You request a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+        `
+      });
+    })
+    .catch(err => console.log(err));
+  })
 };
