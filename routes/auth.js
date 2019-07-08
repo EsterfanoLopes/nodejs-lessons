23fdc48447b6c8b51ba78fc8d000a43f5c4ab5f4
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-07-08 16:37:14 -0300

    Lecture 292. Checking for field equality

--- routes/auth.js
+++ routes/auth.js
@@ -20,6 +20,13 @@ router.post(
     body('password', 'Please enter a password with only numbers and text and at least 5 characters')
       .isLength({ min: 5 })
       .isAlphanumeric(),
+    body('confirmPassword')
+      .custom(( value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!')
+        }
+        return true;
+      }),
   ],
   authController.postSignup);
 
