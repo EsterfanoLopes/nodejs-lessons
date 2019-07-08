e897487822c009ed58f5e73706ad80f1ea208711
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-07-08 16:13:36 -0300

    Lecture 288. Setup and basic validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -78,6 +79,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const { errors } = validationResult(req);
+  if (!errors.isEmpty) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors,
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
