911ac0d00927f8549579799ccee72e4afc4e77ed
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-07-08 16:31:46 -0300

    Lecture 291. More Validatiors

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check')
+const { check, body } = require('express-validator/check')
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,17 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid e-mail.'), authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid e-mail.'),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters')
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
