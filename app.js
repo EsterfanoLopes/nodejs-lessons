bea1e12b35d883dfb82f4a89fcaf708f16160c93
Esterfano Lopes <esterfano.lopes@gmail.com>
2019-07-27 13:33:33 -0300

    Lecture 306. Throwing Errors in Code

--- app.js
+++ app.js
@@ -51,10 +51,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
