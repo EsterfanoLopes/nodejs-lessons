d81da6645cf3dd65c66edd0012699b0adcbde547
Esterfano Lopes <esterfano.lopes@gmail.com>
2019-07-28 13:02:49 -0300

    Lecture 319. Configuring Multer to Adjust Filename & Filepath

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
+});
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -36,7 +45,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: 'images' }).single('image'));
+app.use(multer({ storage: fileStorage }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
