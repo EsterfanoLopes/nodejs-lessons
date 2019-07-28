71966d5fdd71c596d56c5b3581208020e99c691a
Esterfano Lopes <esterfano.lopes@gmail.com>
2019-07-28 12:37:56 -0300

    Lecture 317. Handling Multipart Form data

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,7 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.body.image;
   const price = req.body.price;
   const description = req.body.description;
   const { errors } = validationResult(req);
--- routes/admin.js
+++ routes/admin.js
@@ -22,8 +22,6 @@ router.post(
       .isString()
       .isLength({ min: 3})
       .trim(),
-    body('imageUrl')
-      .isURL(),
     body('price')
       .isFloat(),
     body('description')
