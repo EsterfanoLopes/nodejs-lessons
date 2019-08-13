1288b7393a4a3725fc608389b5b3e2eb953fc5d6
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-12 23:37:24 -0300

    Lecture 327. Restricting File Access

--- controllers/shop.js
+++ controllers/shop.js
@@ -122,15 +122,23 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const invoiceName = 'invoice-' + orderId + '.pdf';
-  const invoicePath = path.join('data', 'invoices', invoiceName);
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err);
+  Order.findById(orderId).then(order => {
+    if (!order) {
+      return next(new Error('No order found.'));
     }
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
-    res.send(data);
-  });
-
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error('Unauthorized'));
+    }
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+      res.send(data);
+    });
+  })
+    .catch(err => next(err));
 };
