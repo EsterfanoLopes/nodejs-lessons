8bac59c55f489c0010b944f14b59dbc5ad6d2518
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-21 22:55:56 -0300

    Lecture 348. Sending and Handling Background Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -180,8 +180,8 @@ exports.getProducts = (req, res, next) => {
     .catch(err => errorHandlerObjectWrapper(500, err));
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId).then(product => {
     if (!product) {
       next(new Error('Product not found!'));
@@ -191,7 +191,9 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then(() => {
     console.log('DESTROYED PRODUCT');
-    res.redirect('/admin/products');
+    res.status(200).json({ message: 'Success!' });
   })
-  .catch(err => errorHandlerObjectWrapper(500, err));
+  .catch(err => {
+    res.status(500).json({ message: 'Deleting product failed.' });
+  });
 };
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,13 @@
-const deleteProduct = () => {
-  const prodId = this.parentNode.querySelector('[name=productId]').value;
-  const csrf = this.parentNode.querySelector('[name=_csrf]').value;
+const deleteProduct = (btn) => {
+  const prodId = btn.parentNode.querySelector('[name=productId]').value;
+  const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+  fetch(`/admin/product/${prodId}`, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrf,
+    }
+  }).then(result => {
+    console.log(result);
+  }).catch(err => console.log(err));
 };
