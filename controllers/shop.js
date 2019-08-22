7ec7617a910362cb8e4e00f8e8cd21bbfbd431cd
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-21 22:14:53 -0300

    Lecture 340. Adding Dynamic Pagination Buttons

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,7 +7,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITENS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -36,22 +36,22 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
   let totalItems;
 
   Product.find()
-    .count()
+    .countDocuments()
     .then(numProducts => {
       totalItems = numProducts;
       return Product.find()
-        .skip((parseInt(page) - 1) * parseInt(ITENS_PER_PAGE))
-        .limit(ITENS_PER_PAGE);
+        .skip((page - 1) * parseInt(ITEMS_PER_PAGE))
+        .limit(ITEMS_PER_PAGE);
     }).then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
