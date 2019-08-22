41986e4bb57b1de4d67bb68fb88182fdbd9e9887
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-21 23:07:47 -0300

    manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,8 @@ const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+  const productElement = btn.closest('article');
+
   fetch(`/admin/product/${prodId}`, {
     method: 'DELETE',
     headers: {
@@ -9,5 +11,9 @@ const deleteProduct = (btn) => {
     }
   }).then(result => {
     console.log(result);
+  }).then(data => {
+    console.log(data);
+    // productElement.remove(); Not allowed in IE
+    productElement.parentNode.removeChild(productElement);
   }).catch(err => console.log(err));
 };
