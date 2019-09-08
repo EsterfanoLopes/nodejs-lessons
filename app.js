6524c62dcd4e60f6364d04065044c33fdd1d103d
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-09-08 19:54:41 -0300

    Lecture 450. Compressing assets

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
@@ -58,6 +59,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(helmet());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
