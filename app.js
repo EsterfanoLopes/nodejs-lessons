6d4fcc3b10b5e910eb6a62323d0e89c214c1daf6
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-09-08 19:52:09 -0300

    Lecture 449. Setting Secure Responser Headers with Helmet

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
@@ -55,6 +56,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
