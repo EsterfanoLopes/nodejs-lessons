c3d251ed8494f1cdb88b9e6c068903d50221d3dd
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-04-05 22:43:57 -0300

    Section 32. More on ES Modules

--- response-handler.js
+++ response-handler.js
@@ -1,9 +1,14 @@
-import fs from 'fs';
+//import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
 
 export const resHandler = (req, res, next) => {
-  fs.readFile('my-page.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
+  // fs.readFile('my-page.html', 'utf8', (err, data) => {
+  //   res.send(data);
+  // });
+  res.sendFile(join(dirname(__filename), 'my-page.html'));
 }
 
 export default resHandler;
