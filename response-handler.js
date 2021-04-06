e0417c1c74368e3c6e732a748a2a7246b1243163
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-04-05 22:48:16 -0300

    Section 32. Node Core Modules & Promises

--- response-handler.js
+++ response-handler.js
@@ -1,14 +1,17 @@
-//import fs from 'fs';
+import fs from 'fs/promises';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
 export const resHandler = (req, res, next) => {
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //   res.send(data);
-  // });
-  res.sendFile(join(dirname(__filename), 'my-page.html'));
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    }).catch(err => {
+      console.log(err);
+    });
+  // res.sendFile(join(dirname(__filename), 'my-page.html'));
 }
 
 export default resHandler;
