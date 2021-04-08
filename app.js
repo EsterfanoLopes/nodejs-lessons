6fccd470a5937421b9e6848ce72b8a5f247807b7
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-04-07 22:39:10 -0300

    Section 33. Working with union types

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+"use strict";
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + '' + num2;
+    }
+    return +num1 + +num2;
 }
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
-    console.log(result);
-});
+if (buttonElement) {
+    buttonElement.addEventListener('click', function () {
+        var num1 = num1Element.value;
+        var num2 = num2Element.value;
+        var result = add(+num1, +num2);
+        var stringResult = add(num1, num2);
+        console.log(result);
+        console.log(stringResult);
+        //console.log(add(true, false));
+    });
+}
--- app.ts
+++ app.ts
@@ -2,8 +2,16 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button');
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  }
+
+  if (typeof num1 === 'string' && typeof num2 === 'string') {
+    return num1 + '' + num2;
+  }
+
+  return +num1 + +num2;
 }
 
 if (buttonElement) {
@@ -11,6 +19,9 @@ if (buttonElement) {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
     console.log(result)
+    console.log(stringResult)
+    //console.log(add(true, false));
   });
 }
