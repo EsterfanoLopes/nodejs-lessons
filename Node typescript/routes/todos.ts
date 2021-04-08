5e915fa918d5fe21fd6867f8e45895d38189a74d
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-04-07 23:24:30 -0300

    Section 33. Finishing the REST routes

--- Node typescript/routes/todos.ts
+++ Node typescript/routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import Todo from '../models/todo';
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -17,6 +17,24 @@ router.post('/todo', (req, res, next) => {
   };
 
   todos.push(newTodo);
+
+  res.status(201).json({ message: 'Added Todo', todo: newTodo, todos });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex], text: req.body.text }
+    return res.status(200).json({ message: 'Updated todo', todos })  
+  }
+
+  res.status(404).json({ message: 'Could not find todo for this id.' })
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+  todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+  res.status(200).json({ message: 'Deleted todo', todos });
 });
 
 export default router;
