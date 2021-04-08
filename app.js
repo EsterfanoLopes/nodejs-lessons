e0f6ad0d02353948542278d121b1555b122ccc0d
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-04-07 22:31:52 -0300

    Session 33. Type inference & type casting

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
-function add(num1, num2) {
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
-
-console.log(add('1', '6'));
+buttonElement.addEventListener('click', () => {
+  const num1 = num1Element.value;
+  const num2 = num2Element.value;
+  const result = add(+num1, +num2);
+  console.log(result)
+});
